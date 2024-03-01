3332f9be267c83366c0471b9b664844f21a53f63
Biswajit Debnath <biswajit.basanti@gmail.com>
2024-03-02 00:05:53 +0530

    fix:
    hfhf

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -10,18 +10,18 @@ const Search = async () => {
   return (
     <div className="mx-4 lg:mx-20">
       {/* topbar */}
-     <div className="flex flex-col gap-5 mt-10">
-        <div className="p-1 sm:p-0 flex flex-col sm:flex-row gap-2 bg-green-300 rounded-3xl lg:rounded-full justify-between">
-          <SearchBox cross={true} width="w-full lg:w-full" />
-          <div className="buttonHandler flex items-center ">
-            <Link href="/people" className="bg-white px-4 py-1 rounded-full">People</Link>
-            <Link href="/people" className="px-4 py-1 rounded-full">Service</Link>
+     <div className="mt-10 flex flex-col gap-5">
+        <div className="flex flex-col justify-between gap-2 rounded-3xl bg-green-300 p-1 sm:flex-row sm:p-0 lg:rounded-full">
+          <SearchBox placeholder="Search..." width="w-full lg:w-full" />
+          <div className="buttonHandler flex items-center">
+            <Link href="/people" className="rounded-full bg-white px-4 py-1">People</Link>
+            <Link href="/people" className="rounded-full px-4 py-1">Service</Link>
           </div>
         </div>
-        <span className="gap-2 flex ">
-          <button className="border border-gray-400 p-2 text-[12px] font-semibold text-gray-700 rounded-xl">Location</button>
-          <button className="border border-gray-400 p-2 text-[12px] font-semibold text-gray-700 rounded-xl">Rate</button>
-          <button className="border border-gray-400 p-2 text-[12px] font-semibold text-gray-700 rounded-xl">All Filters</button>
+        <span className="flex gap-2">
+          <button className="rounded-xl border border-gray-400 p-2 text-[12px] font-semibold text-gray-700">Location</button>
+          <button className="rounded-xl border border-gray-400 p-2 text-[12px] font-semibold text-gray-700">Rate</button>
+          <button className="rounded-xl border border-gray-400 p-2 text-[12px] font-semibold text-gray-700">All Filters</button>
         </span>
      </div>
       
